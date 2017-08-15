64fab574f2d98851c51f1553a090aee85e0de70a
YaChun07 <yachun07@gmail.com>
2017-08-15 10:47:37 +0800

    Use subString to split the band name
    Use subString to split the band name and try to extract the method from the class.
    If the last char is the same as the first letter, repet the char from second to last one.
    If the last char is different from the first one, add The before the str.

---
+++ HWBandName/HWBandName/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWBandName
+{
+    public class Class1
+    {
+        public static string BandNameGenerator(string str)
+        {
+            if (str.Trim().Length==0 || string.IsNullOrEmpty(str))
+            {
+                return "Null";
+            }
+
+            string lastChar = str.Substring(str.Length - 1, 1).ToUpper();
+            string firstChar = str.Substring(0, 1).ToUpper();
+            string bandName = str;
+
+            if (lastChar == firstChar)
+            {
+                bandName = HasSameLetter(str);
+            }
+            else
+            {
+                bandName = NoSameLetter(str);
+            }
+            return bandName;
+        }
+
+        private static string HasSameLetter(string str)
+        {
+            str = toUpperTitle(str) + str.Substring(1, str.Length - 1);
+            return str;
+        }
+
+        private static string NoSameLetter(string str)
+        {
+            str = "The " + toUpperTitle(str);
+            return str;
+        }
+
+        private static string toUpperTitle(string str)
+        {
+            str = str.Substring(0, 1).ToUpper() + str.Substring(1, str.Length - 1);
+            return str;
+        }
+    }
+}
---
+++ HWBandName/HWBandNameTests/Class1Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HWBandName;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace HWBandName.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void NoSameLetter()
+        {
+            Assert.AreEqual("The Knife", Class1.BandNameGenerator("knife"));
+            Assert.AreEqual("The Bed", Class1.BandNameGenerator("bed"));
+            Assert.AreEqual("The Good", Class1.BandNameGenerator("Good"));
+        }
+        [TestMethod()]
+        public void HasSameLetter()
+        {
+            Assert.AreEqual("Tartart", Class1.BandNameGenerator("Tart"));
+            Assert.AreEqual("Sandlesandles", Class1.BandNameGenerator("sandles"));
+        }
+        [TestMethod()]
+        public void NullLetter()
+        {
+            Assert.AreEqual("Null", Class1.BandNameGenerator(" "));
+            Assert.AreEqual("Null", Class1.BandNameGenerator(""));
+        }
+    }
+
+}
