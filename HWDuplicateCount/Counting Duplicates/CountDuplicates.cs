272b593530de731f43ac0c51237953e3e0e9aa41
YaChun07 <yachun07@gmail.com>
2017-08-28 00:28:07 +0800

    use linq to count duplicate letter

---
+++ HWDuplicateCount/Counting Duplicates/CountDuplicates.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Counting_Duplicates
+{
+    public class CountDuplicates
+    {
+        public static int DuplicateCount(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+            str = str.ToUpper();
+            
+            var duplicateCount = str.GroupBy(i => i).Where(g => g.Count() > 1).Count();
+
+            return duplicateCount;
+        }
+    }
+}
---
+++ HWDuplicateCount/Counting DuplicatesTests/CountDuplicatesTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Counting_Duplicates.Tests
+{
+    [TestClass()]
+    public class CountDuplicatesTests
+    {
+        [TestMethod()]
+        public void answer_0_when_stringisnull()
+        {
+            Assert.AreEqual(0, CountDuplicates.DuplicateCount(""));
+        }
+        [TestMethod()]
+        public void answer_2_when_two_duplicate()
+        {
+            Assert.AreEqual(2, CountDuplicates.DuplicateCount("aabbcde"));
+        }
+        [TestMethod()]
+        public void answer_0_when_no_duplicate()
+        {
+            Assert.AreEqual(0, CountDuplicates.DuplicateCount("abcd"));
+        }
+        [TestMethod()]
+        public void answer_2_when_two_duplicate_and_one_upper_case()
+        {
+            Assert.AreEqual(2, CountDuplicates.DuplicateCount("aabBcde"), "should ignore case");
+        }
+
+        [TestMethod()]
+        public void answer_1_when_one_duplicate_and_one_upper_case_vacabulary()
+        {
+            Assert.AreEqual(1, CountDuplicates.DuplicateCount("Indivisibility"));
+        }
+
+        [TestMethod()]
+        public void answer_2_when_two_duplicate_and_one_upper_case_vacabulary()
+        {
+            Assert.AreEqual(2, CountDuplicates.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
