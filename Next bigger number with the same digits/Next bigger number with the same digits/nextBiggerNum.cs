9a2851ca6bcfba24fbf3e69c1bbefda3bfeae609
YaChun07 <yachun07@gmail.com>
2017-10-15 00:02:39 +0800

    [What] 1.find out the left smaller than right 2.sort the right side 3.swap

---
+++ Next bigger number with the same digits/Next bigger number with the same digits/nextBiggerNum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_bigger_number_with_the_same_digits
+{
+    public class nextBiggerNum
+    {
+
+        public static long NextBiggerNumber(long n)
+        {
+            char[] num = n.ToString().ToCharArray();
+            string result = "";
+            for (int i = num.Length - 2; i >= 0; i--)
+            {
+                if (num[i] < num[i + 1])
+                {
+                    char[] numLeft = n.ToString().Substring(0, i + 1).ToCharArray();
+                    char[] numRight = n.ToString().Substring(i + 1, num.Length - i - 1).ToCharArray();
+                    Array.Sort(numRight);
+                    for (int j = 0; j < numRight.Length; j++)
+                    {
+                        
+                        if (numLeft[i] < numRight[j])
+                        {
+                            numLeft[i] = numRight[j];
+                            numRight[j] = num[i];
+                            result = new string(numLeft) + new string(numRight);
+                            return Convert.ToInt64(result);
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+
+
+    }
+}
---
+++ Next bigger number with the same digits/Next bigger number with the same digitsTests/nextBiggerNumTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Next_bigger_number_with_the_same_digits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_bigger_number_with_the_same_digits.Tests
+{
+    [TestClass()]
+    public class nextBiggerNumTests
+    {
+        [TestMethod()]
+        public void two_digit_no_repeat()
+        {
+            Assert.AreEqual(21, nextBiggerNum.NextBiggerNumber(12));
+        }
+        [TestMethod()]
+        public void three_digit_no_repeat()
+        {
+            Assert.AreEqual(531, nextBiggerNum.NextBiggerNumber(513));
+        }
+        [TestMethod()]
+        public void three_digit_with_first_and_second_repeat()
+        {
+            Assert.AreEqual(441, nextBiggerNum.NextBiggerNumber(414));
+        }
+        [TestMethod()]
+        public void three_digit_with_first_and_third_repeat()
+        {
+            Assert.AreEqual(414, nextBiggerNum.NextBiggerNumber(144));
+        }
+        [TestMethod()]
+        public void four_digit_no_repeat()
+        {
+            Assert.AreEqual(2071, nextBiggerNum.NextBiggerNumber(2017));
+        }
+        [TestMethod()]
+        public void four_digit_no_repeat_case2()
+        {
+            Assert.AreEqual(2716, nextBiggerNum.NextBiggerNumber(2671));
+        }
+        [TestMethod()]
+        public void four_digit_with_no_repeat_case3()
+        {
+            Assert.AreEqual(2113, nextBiggerNum.NextBiggerNumber(1321));
+        }
+        [TestMethod()]
+        public void four_digit_with_two_repeat()
+        {
+            Assert.AreEqual(2707, nextBiggerNum.NextBiggerNumber(2077));
+        }
+    }
+}
