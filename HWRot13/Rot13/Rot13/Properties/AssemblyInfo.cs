00cca2b08b079e222bcad640d5ec47ea649ad6e8
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-02-25 21:34:14 +0800

    Decode by Rot13

---
+++ HWRot13/Rot13/Rot13/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rot13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rot13")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6c52b69-d306-483b-bf7a-0c9933efe501")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HWRot13/Rot13/Rot13/Rot13Test.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rot13
+{
+    [TestClass]
+    public class Rot13Test
+    {
+        [TestMethod]
+        public void TestExample()
+        {
+            Assert.AreEqual("ROT13 example.", decode.Rot13("EBG13 rknzcyr."));
+        }
+
+        [TestMethod]
+        public void TestExample1()
+        {
+            Assert.AreEqual("This is my first ROT13 excercise!" , decode.Rot13("Guvf vf zl svefg EBG13 rkprepvfr!"));
+        }
+        [TestMethod]
+        public void String_with_symbol()
+        {
+            Assert.AreEqual("Hi! This is Amanda~", decode.Rot13("Uv! Guvf vf Nznaqn~"));
+        }
+
+        [TestMethod]
+        public void Null_point()
+        {
+            Assert.AreEqual("Not Exist", decode.Rot13(""));
+        }
+    }
+
+    internal class decode
+    {
+        public static string Rot13(string input)
+        {
+            Dictionary<char, char> letter = new Dictionary<char, char>()
+            {
+                { 'a','n'},
+                { 'b','o'},
+                { 'c','p'},
+                { 'd','q'},
+                { 'e','r'},
+                { 'f','s'},
+                { 'g','t'},
+                { 'h','u'},
+                { 'i','v'},
+                { 'j','w'},
+                { 'k','x'},
+                { 'l','y'},
+                { 'm','z'},
+                { 'n','a'},
+                { 'o','b'},
+                { 'p','c'},
+                { 'q','d'},
+                { 'r','e'},
+                { 's','f'},
+                { 't','g'},
+                { 'u','h'},
+                { 'v','i'},
+                { 'w','j'},
+                { 'x','k'},
+                { 'y','l'},
+                { 'z','m'},
+
+            };
+
+            string rotString = "";
+            if (String.IsNullOrEmpty(input))
+            {
+                return "Not Exist";
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (letter.ContainsKey(input.ToLower()[i]))
+                {
+                    char inputChar = input[i];
+                   
+                    if (Char.IsUpper(inputChar))
+                    {
+                        rotString += Char.ToUpper(letter[Char.ToLower(inputChar)]);
+                    }
+                    else
+                    {
+                        rotString += letter[inputChar];
+                    }
+                }
+                else
+                {
+                    rotString += input[i];
+                }
+                
+            }
+            return rotString;
+        }
+    }
+}
