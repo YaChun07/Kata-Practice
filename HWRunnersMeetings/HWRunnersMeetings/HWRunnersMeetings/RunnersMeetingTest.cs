b636979c4bfd0fc54b8a06ad10e8eed36cbd9abe
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-03-18 22:28:58 +0800

    Runners Meetings

---
+++ HWRunnersMeetings/HWRunnersMeetings/HWRunnersMeetings/RunnersMeetingTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HWRunnersMeetings
+{
+    [TestClass]
+    public class RunnersMeetingTest
+    {
+        [TestMethod]
+        public void RunnersMeetingsTest_with_last_Winner()
+        {
+            Assert.AreEqual(3, RunnersMeetings.CountMeetPeople(new int[] { 1, 4, 2 }, new int[] { 27, 18, 24 }));
+        }
+
+        [TestMethod]
+        public void RunnersMeetingsTest_with_second_Winner()
+        {
+            Assert.AreEqual(2, RunnersMeetings.CountMeetPeople(new int[] { 1, 4, 2 }, new int[] { 5, 6, 2 }));
+        }
+
+        [TestMethod]
+        public void RunnersMeetingsTest_with_no_winner()
+        {
+            Assert.AreEqual(-1, RunnersMeetings.CountMeetPeople(new int[] { 1, 2, 3 }, new int[] { 1, 1, 1 }));
+        }
+
+        [TestMethod]
+        public void RunnersMeetingsTest_with_two_player()
+        {
+            Assert.AreEqual(2, RunnersMeetings.CountMeetPeople(new int[] { 1, 1000 }, new int[] { 23, 22 }));
+        }
+    }
+}
---
+++ HWRunnersMeetings/HWRunnersMeetings/HWRunnersMeetings/RunnersMeetings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HWRunnersMeetings
+{
+    internal class RunnersMeetings
+    {
+        public static int CountMeetPeople(int[] startPosition, int[] speed)
+        {
+            var positionList = new List<float>();
+            var speedList = new List<int>(speed);
+            var time = 0;
+            var maxPersonCount = 0;
+            while (speedList.Count > 1)
+            {
+                for (var i = 0; i < speedList.Count; i++)
+                {
+                    positionList.Add((speedList[i] * time) / 60 + startPosition[i]);
+                }
+                var personcount = positionList.GroupBy(x => x).Max(x => x.Count());
+
+                if (personcount > maxPersonCount)
+                {
+                    maxPersonCount = personcount;
+                }
+                var firstRunnerIndex = positionList.IndexOf(positionList.Max());
+
+                if (speedList.Max() == speedList[firstRunnerIndex])
+                {
+                    speedList.RemoveAt(firstRunnerIndex);
+                }
+                time++;
+                positionList.Clear();
+            }
+            return maxPersonCount <= 1 ? -1 : maxPersonCount;
+        }
+    }
+}
