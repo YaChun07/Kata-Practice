7759fd29f519945b9c49e5f208f6c54e56ce37be
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-04-02 02:17:29 +0800

    Use Dictionary to refactor

--- HWPlayDarts/HWPlayDarts/PlayDartsGame.cs
+++ HWPlayDarts/HWPlayDarts/PlayDartsGame.cs
@@ -7,74 +7,74 @@ namespace HWPlayDarts
     {
         public static string GetScore(double x, double y)
         {
-            Dictionary<int, int> scoreTable = new Dictionary<int, int>
-                {
-                    {0,13},
-                    {1,4},
-                    {2,18},
-                    {3,1},
-                    {4,20},
-                    {5,5},
-                    {6,12},
-                    {7,9},
-                    {8,14},
-                    {9,11},
-                    {10,8},
-                    {11,16},
-                    {12,7},
-                    {13,19},
-                    {14,3},
-                    {15,17},
-                    {16,2},
-                    {17,15},
-                    {18,10},
-                    {19,6}
-                };
+            var scoreTable = new Dictionary<int, int>
+            {
+                {0,13},
+                {1,4},
+                {2,18},
+                {3,1},
+                {4,20},
+                {5,5},
+                {6,12},
+                {7,9},
+                {8,14},
+                {9,11},
+                {10,8},
+                {11,16},
+                {12,7},
+                {13,19},
+                {14,3},
+                {15,17},
+                {16,2},
+                {17,15},
+                {18,10},
+                {19,6}
+            };
 
             var angle = Math.Atan2(y, x) / Math.PI * 180;
             if (angle < 0)
             {
                 angle += 360;
             }
+
             var period = (angle - 9) / 18;
             if (period < 0)
             {
                 period = 19;
             }
-
-            return GetResult(x * x + y * y, scoreTable[(int)period]);
+            var distance = x * x + y * y;
+            return GetResult(distance, scoreTable[(int)period]);
         }
 
         private static string GetResult(double distance, int score)
         {
-            if (distance < Math.Pow(12.70 / 2, 2))
+            var radiusPeriod = new Dictionary<double, string>
             {
-                return "DB";
-            }
-            else if (distance < Math.Pow(31.8 / 2, 2))
+                {RadiusSquare(12.70),"DB" },
+                {RadiusSquare(31.8),"SB"},
+                {RadiusSquare(198),""},
+                {RadiusSquare(214),"T"},
+                {RadiusSquare(324),"" },
+                {RadiusSquare(340),"D"}
+            };
+
+            foreach (var radius in radiusPeriod)
             {
-                return "SB";
+                if (distance < radius.Key)
+                {
+                    if (radius.Value.Length == 2)
+                    {
+                        return radius.Value;
+                    }
+                    return radius.Value + score;
+                }
             }
-            else if (distance < Math.Pow(198 / 2, 2))
-            {
-                return "" + score;
-            }
-            else if (distance < Math.Pow(214 / 2, 2))
-            {
-                return "T" + score;
-            }
-            else if (distance < Math.Pow(324 / 2, 2))
-            {
-                return "" + score;
-            }
-            else if (distance < Math.Pow(340 / 2, 2))
-            {
-                return "D" + score;
-            }
-            else
-            {
-                return "X";
-            }
+            return "X";
+        }
+
+        private static double RadiusSquare(double radius)
+        {
+            return Math.Pow(radius / 2, 2);
         }
     }
 }
