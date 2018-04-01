0960736af626316305426de033566dcab4668c58
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-04-02 02:08:43 +0800

    Let's Play Darts!

---
+++ HWPlayDarts/HWPlayDarts/PlayDartsGame.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HWPlayDarts
+{
+    internal class PlayDartsGame
+    {
+        public static string GetScore(double x, double y)
+        {
+            Dictionary<int, int> scoreTable = new Dictionary<int, int>
+                {
+                    {0,13},
+                    {1,4},
+                    {2,18},
+                    {3,1},
+                    {4,20},
+                    {5,5},
+                    {6,12},
+                    {7,9},
+                    {8,14},
+                    {9,11},
+                    {10,8},
+                    {11,16},
+                    {12,7},
+                    {13,19},
+                    {14,3},
+                    {15,17},
+                    {16,2},
+                    {17,15},
+                    {18,10},
+                    {19,6}
+                };
+
+            var angle = Math.Atan2(y, x) / Math.PI * 180;
+            if (angle < 0)
+            {
+                angle += 360;
+            }
+            var period = (angle - 9) / 18;
+            if (period < 0)
+            {
+                period = 19;
+            }
+
+            return GetResult(x * x + y * y, scoreTable[(int)period]);
+        }
+
+        private static string GetResult(double distance, int score)
+        {
+            if (distance < Math.Pow(12.70 / 2, 2))
+            {
+                return "DB";
+            }
+            else if (distance < Math.Pow(31.8 / 2, 2))
+            {
+                return "SB";
+            }
+            else if (distance < Math.Pow(198 / 2, 2))
+            {
+                return "" + score;
+            }
+            else if (distance < Math.Pow(214 / 2, 2))
+            {
+                return "T" + score;
+            }
+            else if (distance < Math.Pow(324 / 2, 2))
+            {
+                return "" + score;
+            }
+            else if (distance < Math.Pow(340 / 2, 2))
+            {
+                return "D" + score;
+            }
+            else
+            {
+                return "X";
+            }
+        }
+    }
+}
---
+++ HWPlayDarts/HWPlayDarts/PlayDartsGameTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HWPlayDarts
+{
+    [TestClass]
+    public class PlayDartsGameTest
+    {
+        [TestMethod]
+        public void OutSideTest()
+        {
+            var actual = PlayDartsGame.GetScore(-133.690, -147.380);
+            Assert.AreEqual("X", actual);
+        }
+
+        [TestMethod]
+        public void BullEye()
+        {
+            var actual = PlayDartsGame.GetScore(4.06, 0.71);
+            Assert.AreEqual("DB", actual);
+        }
+
+        [TestMethod]
+        public void Bull()
+        {
+            var actual = PlayDartsGame.GetScore(2.38, -6.06);
+            Assert.AreEqual("SB", actual);
+        }
+
+        [TestMethod]
+        public void SingleNumber_20()
+        {
+            var actual = PlayDartsGame.GetScore(-5.43, 117.95);
+            Assert.AreEqual("20", actual);
+        }
+
+        [TestMethod]
+        public void SingleNumber_7()
+        {
+            var actual = PlayDartsGame.GetScore(-73.905, -95.94);
+            Assert.AreEqual("7", actual);
+        }
+
+        [TestMethod]
+        public void tripleNumber_2()
+        {
+            var actual = PlayDartsGame.GetScore(55.53, -87.95);
+            Assert.AreEqual("T2", actual);
+        }
+
+        [TestMethod]
+        public void doubleNumber_9()
+        {
+            var actual = PlayDartsGame.GetScore(-145.19, 86.53);
+            Assert.AreEqual("D9", actual);
+        }
+    }
+}
