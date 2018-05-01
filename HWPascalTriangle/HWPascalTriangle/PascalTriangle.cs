2c7edc38d0d5d3aa3c90d8b5e9e0404d7404e47f
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-05-01 22:19:38 +0800

    Pascal Triangle

---
+++ HWPascalTriangle/HWPascalTriangle/PascalTriangle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace HWPascalTriangle
+{
+    public static class PascalTriangle
+    {
+        public static List<int> CalcualteLayer(int totalLayer)
+        {
+            var ints = new List<int>();
+
+            for (var i = 1; i <= totalLayer; i++)
+            {
+                ints.AddRange(PascalsTriangle(i));
+            }
+            return ints;
+        }
+
+        private static List<int> PascalsTriangle(int totalLayer)
+        {
+            if (totalLayer == 1)
+            {
+                return new List<int> { 1 };
+            }
+            else if (totalLayer == 2)
+            {
+                return new List<int> { 1, 1 };
+            }
+            else
+            {
+                var list = new List<int> { 1 };
+                var ints = PascalsTriangle(totalLayer - 1);
+                for (var i = 0; i < ints.Count - 1; i++)
+                {
+                    list.Add(ints[i] + ints[i + 1]);
+                }
+                list.Add(1);
+                return list;
+            }
+        }
+    }
+}
---
+++ HWPascalTriangle/HWPascalTriangle/PascalTriangleTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HWPascalTriangle
+{
+    [TestClass]
+    public class PascalTriangleTest
+    {
+        [TestMethod]
+        public void One_Layer()
+        {
+            var actual = PascalTriangle.CalcualteLayer(1);
+            CollectionAssert.AreEqual(new List<int> { 1 }, actual);
+        }
+
+        [TestMethod]
+        public void Two_Layer()
+        {
+            var actual = PascalTriangle.CalcualteLayer(2);
+            CollectionAssert.AreEqual(new List<int> { 1, 1, 1 }, actual);
+        }
+
+        [TestMethod]
+        public void Three_Layer()
+        {
+            var actual = PascalTriangle.CalcualteLayer(3);
+            CollectionAssert.AreEqual(new List<int> { 1, 1, 1, 1, 2, 1 }, actual);
+        }
+
+        [TestMethod]
+        public void Four_Layer()
+        {
+            var actual = PascalTriangle.CalcualteLayer(4);
+            CollectionAssert.AreEqual(new List<int> { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1 }, actual);
+        }
+
+        [TestMethod]
+        public void Five_Layer()
+        {
+            var actual = PascalTriangle.CalcualteLayer(5);
+            CollectionAssert.AreEqual(new List<int> { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1 }, actual);
+        }
+
+        [TestMethod]
+        public void Six_Layer()
+        {
+            var actual = PascalTriangle.CalcualteLayer(6);
+            CollectionAssert.AreEqual(new List<int> { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1, 1, 5, 10, 10, 5, 1 }, actual);
+        }
+    }
+}
