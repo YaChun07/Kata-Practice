5c0c948fc6a3b123f5aa774f68c32b24219a8181
amanda.yang93 <amanda.yang@titansoft.com.sg>
2018-08-12 22:24:15 +0800

    Maximum Length Difference

---
+++ HWMaximumLengthDiff/HWMaximumLengthDiff/MaxDiffLength.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace HWMaximumLengthDiff
+{
+    public class MaxDiffLength
+    {
+        public int Mxdiflg(string[] s1, string[] s2)
+        {
+            if (s1.Length == 0 || s2.Length == 0)
+            {
+                return -1;
+            }
+
+            var s1LengthList = s1.Select(s => s.Length).ToList();
+            var s2LengthList = s2.Select(s => s.Length).ToList();
+
+            var maxValue1 = s1LengthList.Max() - s2LengthList.Min();
+            var maxValue2 = s2LengthList.Max() - s1LengthList.Min();
+            
+            return Math.Max(maxValue1 , maxValue2);
+        }
+    }
+}
---
+++ HWMaximumLengthDiff/HWMaximumLengthDiff/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HWMaximumLengthDiff")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HWMaximumLengthDiff")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad3e70be-bdfd-40f6-9532-a5293834e35d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
